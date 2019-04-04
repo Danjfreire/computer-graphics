237786e3ac33b446bb5e3b1ee752e370f969eb57
Daniel Freire <danjfreire@gmail.com>
2019-04-04 20:54:43 -0300

    Produto vetorial e escalar

--- src/models/Test.java
+++ src/models/Test.java
@@ -25,5 +25,13 @@ public class Test {
         System.out.println("b) ");
         Vector3 result2 = Vector3Operations.getInstance().subtraction(new Vector3(3.5, 1.5, 2), new Vector3(1.0, 2.0, 1.5));
         System.out.println(result2);
+
+        System.out.println("c)");
+        double result3 = Vector3Operations.getInstance().dotProduct(new Vector3(1,2,3), new Vector3(1,5,7));
+        System.out.println(result3);
+
+        System.out.println("d)");
+        Vector3 result4 = Vector3Operations.getInstance().crossProduct(new Vector3(1,2,1), new Vector3(1,0,-1));
+        System.out.println(result4);
     }
 }
--- src/operations/Vector3Operations.java
+++ src/operations/Vector3Operations.java
@@ -30,4 +30,20 @@ public class Vector3Operations {
 
         return this.addition(entry1, negativeEntry2);
     }
+
+    public double dotProduct(Vector3 entry1, Vector3 entry2){
+        double xProduct = entry1.getX() * entry2.getX();
+        double yProduct = entry1.getY() * entry2.getY();
+        double zProduct = entry1.getZ() * entry2.getZ();
+
+        return xProduct + yProduct + zProduct;
+    }
+
+    public Vector3 crossProduct(Vector3 entry1, Vector3 entry2){
+        double xProduct = (entry1.getY()*entry2.getZ()) - (entry1.getZ()*entry2.getY());
+        double yProduct = (entry1.getZ()*entry2.getX()) - (entry1.getX()*entry2.getZ());
+        double zProduct = (entry1.getX()*entry2.getY()) - (entry1.getY()*entry2.getX());
+
+        return new Vector3(xProduct,yProduct,zProduct);
+    }
 }
